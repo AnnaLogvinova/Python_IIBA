93ec2ce34e0dc2522e514f1f2c66ee21be8f3f9b
AnnaLogvinova <99403680+AnnaLogvinova@users.noreply.github.com>
2022-09-03 19:30:37 +0300

    riddles

---
+++ riddles/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Option, Riddle
+from .models import Message
+...
+from .models import Mark
+
+admin.site.register(Riddle)
+admin.site.register(Option)
+
+admin.site.register(Message)
+
+admin.site.register(Mark)
+# Register your models here.
